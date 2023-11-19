bdd03344fff2f7e72cfbc407541767949253beec
Caesar_cipher <lngate@gmail.com>
2023-11-19 14:37:27 +0300

    arranged MVC and created handlebars

---
+++ src/main/java/ke/co/safaricom/Models/materials.java
@@ -0,0 +1,5 @@
+package ke.co.safaricom.Models;
+
+public class materials {
+
+}
--- src/main/java/ke/co/safaricom/controllers/loginController.java
+++ src/main/java/ke/co/safaricom/controllers/loginController.java
@@ -1,46 +1,12 @@
 package ke.co.safaricom.controllers;
-/* below is using servlet instead of spark will have to redo in spark
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
-public class loginController extends HttpServlet {
-    public static void main(String[] args) {
-        @Override
-        protected void doGet (HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-            // Handle the GET request (display the login form)
-            request.getRequestDispatcher("loginForm.html").forward(request, response);
-        }
-
-        @Override
-        protected void doPost (HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-            // Handle the POST request (process login)
-            String email = request.getParameter("email");
-            String password = request.getParameter("password");
-
-            // Implement your user authentication logic here
-            if (authenticateUser(email, password)) {
-                // Successful login, redirect to a success page
-                response.sendRedirect("loginSuccess.html");
-            } else {
-                // Login failed, redirect to an error page or display an error message
-                response.sendRedirect("loginError.html");
-            }
-        }
-
-        // Replace this method with your actual authentication logic
-        private boolean authenticateUser (String email, String password){
-            // Implement user authentication logic here, e.g., check the credentials against your database.
-            // Return true if authentication is successful, otherwise return false.
-            return true; // For demonstration purposes; you should verify user credentials in your actual code.
-        }
-    }
-}*/
+// authenticate user
+//check authorization ie partner or safaricom staff
+// change/reset password
+public class loginController {
 
 
+
+
+
+}
